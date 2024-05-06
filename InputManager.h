48a622bc9f157c484966f2639bc30cd2992bd00f
AmmarAyman <ammar.ayman03@gmail.com>
2024-05-06 21:13:54 +0300

    Editing the input manager Class

--- InputManager.h
+++ InputManager.h
@@ -1,1 +1,19 @@
+#ifndef INPUTMANAGER_H
+#define INPUTMANAGER_H
+#include "BookManager.h"
 
+class InputManager : public BookManager {
+
+public:
+	getData() {
+		cout << "Type The Department: ";
+		cin >> department;
+
+		cout << "Type the Author: ";
+		cin >> author;
+	}
+
+};
+
+
+#endif
